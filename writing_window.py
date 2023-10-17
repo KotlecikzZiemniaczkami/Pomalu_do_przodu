c20e4d81725554e1b712c7b8192f90cd5d781946
KotlecikzZiemniaczkami <127383477+KotlecikzZiemniaczkami@users.noreply.github.com>
2023-10-17 13:28:16 +0200

    new class, which will be able to make 2 very important windows

---
+++ writing_window.py
@@ -0,0 +1,34 @@
+import tkinter
+
+class WritingWindow:
+    def __init__(self, label: str, title: str, writing: str):
+        self.__writing1 = label
+        self.__writing2 = writing
+        self.__title = title
+        self.__ww = 0
+        self.__en = 0
+        self.__id = 0
+
+    # is reading an id which was written by user
+    def giveId(self):
+        self.__id = int(self.en.get())
+
+    # GUI
+    def window(self):
+        # making a root
+        self.__ww = tkinter.Tk()
+        # putting a gui title
+        self.__ww.title(self.__title)
+        # everything what will be on the screen
+        label = tkinter.Label(self.__ww, width=45, bg='red', fg='black', text=self.__writing1)
+        en = tkinter.Entry(self.__ww, width=50, bg='black', fg='red', borderwidth=10)
+        self.en.insert(0, "Id: ")
+        button = tkinter.Button(self.__ww, width=20, bg='red', fg='black', text=self.__writing2, borderwidth=7, command = self.giveId)
+
+        label.grid(row=0)
+        en.grid(row=1)
+        button.grid(row=2)
+
+    # is returning id
+    def getId(self):
+        return self.__id
